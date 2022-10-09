c43647519a0878928d76d878899d80c9088fbc3d
oskark22 <oskar.koskiranta@gmail.com>
2022-10-09 21:07:33 +0300

    Added new method called add

--- src/main/java/ie/atu/Own.java
+++ src/main/java/ie/atu/Own.java
@@ -1,8 +1,22 @@
 package ie.atu;
+import java.util.Scanner;
 
 public class Own {
     public static void main(String[] args) {
+        add();
+    }
 
+    public static void add()
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNum = inputs.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        int secondNum = inputs.nextInt();
+
+        int total = firstNum + secondNum;
+        System.out.println("The total is " + total);
     }
 
 }
